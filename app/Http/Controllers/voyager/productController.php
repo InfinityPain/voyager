a0f85f80fb3f97c54ee348c2d508fc2eaa36a876
christian <cicpotaku6@gmail.com>
2024-03-28 09:33:22 -0400

    se agrega el proyecto

---
+++ app/Http/Controllers/voyager/productController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\voyager;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use TCG\Voyager\Http\Controllers\VoyagerBaseController;
+
+class productController extends VoyagerBaseController
+{
+
+}
